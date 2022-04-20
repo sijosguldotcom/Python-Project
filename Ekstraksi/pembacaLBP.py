4042db99602516c27bf6d56929c767df75ee1f4d
Yosua XG <josua.prima@gmail.com>
2022-04-20 17:48:52 +0700

    nambah ekstraksi

---
+++ Ekstraksi/pembacaLBP.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+   
+import cv2
+image = cv2.imread(r'C:\Users\Si Josgul\Desktop\GitHub\Program\Ekstraksi\GFG.jpg')
+  
+# Now, the variable 'image' stores the pixel values of image
+print(image)
